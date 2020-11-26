a3561b530b5e780994047cb0ed43dee92b70a4b4
clo64 <clo64@scarletmail.rutgers.edu>
2020-11-26 17:53:15 -0500

    Experiment with scraper

---
+++ Scraper/scraper.py
@@ -0,0 +1,37 @@
+##
+#  Charles Owen
+#  Python script for scraping PIP sensor data that has been
+#  saved usig the cat command.
+#  To direct the PIP data into a .txt file, from the 
+#  linux CLI use the command:
+#
+#  sudo stdbuf -o0 ./pip_sense.v2 l l | stdbuf -o0 grep TX:0$1 | cat > sensor_data.txt
+# #
+
+import time
+
+class FileScraper():
+
+    def __init__(self, file_Name):
+        self.file_Name = file_Name
+
+    def extract_lines(self):
+        with open(self.file_Name, 'r') as f:
+            self.content = f.readlines()[-1]
+        return self.content
+
+    def print_all_data_lines(self):
+        if self.content:
+            for line in self.content:
+                print(line)
+
+    def print_last_line(self):
+        print(self.content)
+
+#test code for scraper class
+if __name__ == "__main__":
+    
+    file_Name = input("Please enter the file name: ")
+    scraper = FileScraper(file_Name)
+    scraper.extract_lines()
+    scraper.print_last_line()
