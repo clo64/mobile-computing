48dfe4fbd73a8278b4ccc06e07cfd3816916e6ae
clo64 <clo64@scarletmail.rutgers.edu>
2020-11-29 18:37:36 -0500

    hot fix

--- pip_to_database.py
+++ pip_to_database.py
@@ -1,15 +1,15 @@
 from pymongo import MongoClient
-from Scraper import FileScraper
+from Scraper import scraper
 import time
 
 #connect to mongo and create connection to PIP database
 client = MongoClient('mongodb+srv://cowen:compproject@cluster0.1qgln.mongodb.net/Cluster0?retryWrites=true&w=majority')
-db = client.rooms
+db = client.PIPdata
 
 file_Name = '../PIPtagCode/ReceiverCode/data.txt'
-scraper = FileScraper(file_Name)
-    while True:
-        scraper.extract_line()
-        scraper.print_last_line()
-        scraper.extract_sensor_data()
-        time.sleep(2)
+scraper = scraper.FileScraper(file_Name)
+while True:
+    scraper.extract_line()
+    scraper.print_last_line()
+    scraper.extract_sensor_data()
+    time.sleep(2)
